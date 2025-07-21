76b616ad21b58832cf6464d649e0739ba0da8fc7
PetsuTHEPRO <javalicorp18@gmail.com>
2025-07-21 19:31:10 -0300

    Alterações do github

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/ponte-digital/',
+  base: '/Portal-Central-Digital/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
