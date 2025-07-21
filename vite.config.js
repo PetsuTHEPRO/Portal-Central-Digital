52e3a153c12f306ebc7ceac7c9fd2acb452a0b09
PetsuTHEPRO <javalicorp18@gmail.com>
2025-07-21 19:25:06 -0300

    Configurando para deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/ponte-digital/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
